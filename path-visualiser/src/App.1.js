ca07eb7a674aedd5859d071c863749d51f2758f3
Nomicy <nomicygupta@gmail.com>
2023-10-23 13:53:25 +0530

    Initial commit

---
+++ path-visualiser/src/App.1.js
@@ -0,0 +1,9 @@
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+export function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
---
+++ path-visualiser/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+      {/* <iframe src="structure.html" title='Main html'></iframe> */}
+    </div>
+  );
+}
+
+export default App;
